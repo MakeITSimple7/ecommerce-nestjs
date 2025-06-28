15e6319c2c77f49e063519daa75802f07efb783e
vinh-hoang-ngoc <vinhhoangngoc22@gmail.com>
2025-06-28 10:06:58 +0700

    apply repository pattern

--- src/routes/auth/auth.model.ts
+++ src/routes/auth/auth.model.ts
@@ -1,10 +1,43 @@
-import { Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
-import { RolesService } from './roles.service'
+import { UserStatus } from 'src/shared/constants/auth.constant'
+import { z } from 'zod'
 
-@Module({
-  providers: [AuthService, RolesService],
-  controllers: [AuthController],
+export const UserSchema = z.object({
+  id: z.number(),
+  email: z.string().email(),
+  name: z.string().min(1).max(100),
+  password: z.string().min(6).max(100),
+  phoneNumber: z.string().min(9).max(15),
+  avatar: z.string().nullable(),
+  totpSecret: z.string().nullable(),
+  status: z.nativeEnum(UserStatus),
+  roleId: z.number().positive(),
+  createdById: z.number().nullable(),
+  updatedById: z.number().nullable(),
+  deletedAt: z.date().nullable(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 })
-export class AuthModule {}
+
+export type UserType = z.infer<typeof UserSchema>
+
+export const RegisterBodySchema = UserSchema.pick({
+  email: true,
+  password: true,
+  name: true,
+  phoneNumber: true,
+})
+  .extend({
+    confirmPassword: z.string().min(6).max(100),
+  })
+  .strict()
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Password and confirm password must match',
+        path: ['confirmPassword'],
+      })
+    }
+  })
+
+export type RegisterBodyType = z.infer<typeof RegisterBodySchema>
--- src/routes/auth/auth.module.ts
+++ src/routes/auth/auth.module.ts
@@ -1,10 +1,11 @@
+import { AuthRepository } from './auth.repo'
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { RolesService } from './roles.service'
 
 @Module({
-  providers: [AuthService, RolesService],
+  providers: [AuthService, RolesService, AuthRepository],
   controllers: [AuthController],
 })
 export class AuthModule {}
