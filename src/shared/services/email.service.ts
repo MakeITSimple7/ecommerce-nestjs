6881a21e0a870fa044d9078c79ffef503eeec9b8
vinh-hoang-ngoc <vinhhoangngoc22@gmail.com>
2025-06-28 23:41:31 +0700

    feat(api): create template for sending otp

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from '../config'
+import path from 'path'
+import fs from 'fs'
 
 @Injectable()
 export class EmailService {
@@ -10,11 +12,13 @@ export class EmailService {
   }
 
   sendOTP(payload: { email: string; code: string }) {
+    const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), 'utf-8')
+    const subject = 'OTP Code'
     return this.resend.emails.send({
-      from: 'Ecommerce <onboarding@resend.dev>',
+      from: 'Ecommerce <no-reply@makeitsimple.io.vn>',
       to: [payload.email],
-      subject: 'OTP Code',
-      html: '<p>Congrats on sending your <strong>first email</strong>!</p>',
+      subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
