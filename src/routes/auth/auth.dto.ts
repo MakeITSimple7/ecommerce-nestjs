14fbe9faec7288eb694bf7eee00fd18b64b6cda7
vinh-hoang-ngoc <vinhhoangngoc22@gmail.com>
2025-07-01 23:25:47 +0700

    feat(api): modify auth.dto

--- src/routes/auth/auth.dto.ts
+++ src/routes/auth/auth.dto.ts
@@ -1,13 +1,27 @@
 import { createZodDto } from 'nestjs-zod'
-import { LoginBodySchema, LoginResSchema, RegisterBodySchema, SendOTPBodySchema } from './auth.model'
-import { UserSchema } from 'src/shared/models/shared-user.model'
+import {
+  LoginBodySchema,
+  LoginResSchema,
+  LogoutBodySchema,
+  RefreshTokenBodySchema,
+  RefreshTokenResSchema,
+  RegisterBodySchema,
+  RegisterResSchema,
+  SendOTPBodySchema,
+} from 'src/routes/auth/auth.model'
 
 export class RegisterBodyDTO extends createZodDto(RegisterBodySchema) {}
 
-export class RegisterResDTO extends createZodDto(UserSchema) {}
+export class RegisterResDTO extends createZodDto(RegisterResSchema) {}
 
 export class SendOTPBodyDTO extends createZodDto(SendOTPBodySchema) {}
 
 export class LoginBodyDTO extends createZodDto(LoginBodySchema) {}
 
 export class LoginResDTO extends createZodDto(LoginResSchema) {}
+
+export class RefreshTokenBodyDTO extends createZodDto(RefreshTokenBodySchema) {}
+
+export class RefreshTokenResDTO extends createZodDto(RefreshTokenResSchema) {}
+
+export class LogoutBodyDTO extends createZodDto(LogoutBodySchema) {}
